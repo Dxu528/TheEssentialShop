02ee8114b1c70b6b0f2a3e0e9853ebcd6cddb0ac
Danny <dxumail.dxu@gmail.com>
2020-11-30 16:09:22 -0500

    Added bottom tab navigation, search screen, user screen, local products screen and cart screen

--- App.js
+++ App.js
@@ -89,7 +89,7 @@ export default function BottomTabs() {
                 },
               })}
               tabBarOptions={{
-                activeTintColor: 'tomato',
+                activeTintColor: 'blue',
                 inactiveTintColor: 'gray',
               }}
                 >
