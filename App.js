84247849c0da3e23429856443a460c962d91a071
Danny <dxumail.dxu@gmail.com>
2020-11-30 15:42:01 -0500

    Added bottom tab navigation, search screen, user screen, local products screen and cart screen

--- App.js
+++ App.js
@@ -1,50 +1,106 @@
 import * as React from 'react';
-import { StyleSheet, View, Text, TouchableHighlight } from 'react-native';
+import { StyleSheet, View, Text, TouchableHighlight, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 function HomeScreen({navigation}) {
   return (
      <View style={{ flex: 1, alignItems:
            'center', justifyContent: 'center' }}>
-        <Text style={{fontSize:30}}>Home Screen</Text>
+        <Text style={{fontSize:30}}>The Essential Shop</Text>
         <Text>{'\n'}</Text>
-        <TouchableHighlight
-          onPress={() => navigation.navigate('Details')}
-          activeOpacity={0.6}
-          underlayColor='red'
-        >
-          <Text style={styles.openButton}>Go To Details</Text>
-        </TouchableHighlight>
-
+        <Button
+           title="Search"
+           color="#841584"
+           onPress={() =>
+           <Tab.Screen name="Search" component={SearchScreen} />
+         }/>
      </View>
   );
 }
 
-function DetailsScreen() {
+function SearchScreen() {
      return (
 	<View style={{ flex: 1, alignItems:
 		'center', justifyContent: 'center' }}>
-		<Text>Details Screen</Text>
+		<Text>Search Screen</Text>
 	</View>
      );
 }
 
-const Stack = createStackNavigator();
-
-function App() {
-    return (
-        <NavigationContainer>
-          <Stack.Navigator initialRouteName="Home">
-			      <Stack.Screen name="The Essential Shop" component={HomeScreen} />
-			      <Stack.Screen name="Details" component={DetailsScreen} />
-		      </Stack.Navigator>
-
-	      </NavigationContainer>
-     );
+function UserScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>User Screen</Text>
+    </View>
+  );
 }
 
-export default App;
+function LocalProductsScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Local Products Screen</Text>
+    </View>
+  );
+}
+
+function CartScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Cart Screen</Text>
+      <Button
+        title="Return to Home"
+        color="#841584"
+        onPress={() =>
+            <Tab.Screen name="Home" component={HomeScreen} />
+        }/>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+    	<Stack.Screen name="The Essential Shop" component={HomeScreen} />
+    	<Stack.Screen name="Search" component={SearchScreen} />
+      </Stack.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === 'Home') {
+              iconName = focused
+                ? 'ios-information-circle'
+                : 'ios-information-circle-outline';
+            } else if (route.name === 'User') {
+              iconName = focused ? 'ios-list-box' : 'ios-list';
+            } else if (route.name === 'LocalProducts') {
+              iconName = focused ? 'ios-help-circle' : 'ios-help-circle-outline';
+            } else if (route.name === 'Cart') {
+              iconName = focused ? 'ios-help-circle' : 'ios-help-circle-outline';
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: 'tomato',
+          inactiveTintColor: 'gray',
+        }}
+      >
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="User" component={UserScreen} />
+        <Tab.Screen name="LocalProducts" component={LocalProductsScreen} />
+        <Tab.Screen name="Cart" component={CartScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
 
 const styles = StyleSheet.create({
   openButton: {
